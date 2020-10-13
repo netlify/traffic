6a587e31e203557fc27d0056c52f0b07f812fd02
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2020-10-13 08:39:59 -0700

    ♻ Refactor traffic::uuid to support parsing strings and converting to std::string
    Parsing is a pass/fail operation so we simply use an apex::optional<T>
    for the operation

--- include/traffic/uuid.hpp
+++ include/traffic/uuid.hpp
@@ -1,6 +1,8 @@
 #ifndef TRAFFIC_UUID_HPP
 #define TRAFFIC_UUID_HPP
 
+#include <apex/core/optional.hpp>
+#include <apex/core/string.hpp>
 #include <traffic/memory.hpp>
 #include <ts/ts.h>
 
@@ -12,11 +14,21 @@ template <> struct default_delete<TSUuid> {
 };
 
 struct uuid : private unique_handle<TSUuid> {
+
+  static apex::optional<uuid> parse (std::string const&) noexcept;
+  static apex::optional<uuid> parse (apex::zstring_view) noexcept;
+  static apex::optional<uuid> parse (char const*) noexcept;
+
   using resource_type::resource_type;
+  using resource_type::operator bool;
   using resource_type::get;
+
+  char const* data () const noexcept;
+  size_t size () const noexcept; /* O(n) operation */
 };
 
-std::string_view to_string (uuid) noexcept;
+apex::zstring_view to_string_view (uuid) noexcept;
+std::string to_string (uuid);
 
 } /* namespace traffic */
 
